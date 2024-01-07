bc3ec1f115052fcc55e2be81c1d9e330a2d4d635
7kasper <kasper77077@gmail.com>
2024-01-07 20:25:38 +0100

    Fix stupid error.

--- src/nl/kaspermuller/honeyxp/HoneyXp.java
+++ src/nl/kaspermuller/honeyxp/HoneyXp.java
@@ -29,46 +29,45 @@ public class HoneyXp extends JavaPlugin implements Listener {
 	public void onClick(PlayerInteractEvent e) {
 		if (
 			e.getAction() == Action.RIGHT_CLICK_BLOCK && 
+			e.hasItem() && e.getItem().getType() == Material.HONEY_BOTTLE &&
 			e.getClickedBlock().getType() == Material.LIGHTNING_ROD && 
 			e.getClickedBlock().getRelative(BlockFace.DOWN).getType() == Material.LAPIS_BLOCK
 		) {
-			if (e.getItem().getType() == Material.HONEY_BOTTLE) {
-				ExperienceManager expMan = new ExperienceManager(e.getPlayer());
-				if (e.getPlayer().hasPermission("honeyxp.bottle")) {
-					int points = 7;
-					int onPoints = 0;
-					if (e.getItem().hasItemMeta() && e.getItem().getItemMeta().hasLore()) {
-						try {
-							onPoints = Integer.parseInt(e.getItem().getItemMeta().getLore().get(0).split(": ")[1]);
-							points = expMan.getXpNeededToLevelUp(expMan.getLevelForExp(onPoints) + 1);
-							if (points > 36) { // don't go further than 21 levels (from 0) stored in a bottle.
-								e.getPlayer().getWorld().playSound(e.getPlayer(), Sound.BLOCK_LAVA_POP, 1.0f, 1.0f);
-								return;
-							}
-						} catch (Exception ex) { ex.printStackTrace();}
-					}
-					expMan.changeExp(-points);
-					ItemStack hb = e.getItem();
-					ItemStack newB = hb.clone();
-					// Remove one normal honey bottle.
-					hb.setAmount(hb.getAmount() - 1);
-					// Prepare new Honey Xp Bottle
-					newB.setAmount(1);
-					ItemMeta m = newB.getItemMeta();
-					m.setLore(Arrays.asList("Stored XP: " + (onPoints + points)));
-					m.setDisplayName(ChatColor.RESET + "Honey Xp Bottle");
-					// TODO no hack? For now we just add random enchantment and remove its visisbility:
-					m.addEnchant(Enchantment.THORNS, 1, true);
-					m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-					// Give the item.
-					newB.setItemMeta(m);
-					e.getPlayer().getInventory().addItem(newB);
-					e.getPlayer().updateInventory();
-					e.getPlayer().getWorld().playSound(e.getPlayer(), Sound.BLOCK_BUBBLE_COLUMN_UPWARDS_INSIDE, 1.0f, 1.0f);
-					return;
+			ExperienceManager expMan = new ExperienceManager(e.getPlayer());
+			if (e.getPlayer().hasPermission("honeyxp.bottle")) {
+				int points = 7;
+				int onPoints = 0;
+				if (e.getItem().hasItemMeta() && e.getItem().getItemMeta().hasLore()) {
+					try {
+						onPoints = Integer.parseInt(e.getItem().getItemMeta().getLore().get(0).split(": ")[1]);
+						points = expMan.getXpNeededToLevelUp(expMan.getLevelForExp(onPoints) + 1);
+						if (points > 36) { // don't go further than 21 levels (from 0) stored in a bottle.
+							e.getPlayer().getWorld().playSound(e.getPlayer(), Sound.BLOCK_LAVA_POP, 1.0f, 1.0f);
+							return;
+						}
+					} catch (Exception ex) { ex.printStackTrace();}
 				}
-				e.getPlayer().getWorld().playSound(e.getPlayer(), Sound.BLOCK_LAVA_POP, 1.0f, 1.0f);
+				expMan.changeExp(-points);
+				ItemStack hb = e.getItem();
+				ItemStack newB = hb.clone();
+				// Remove one normal honey bottle.
+				hb.setAmount(hb.getAmount() - 1);
+				// Prepare new Honey Xp Bottle
+				newB.setAmount(1);
+				ItemMeta m = newB.getItemMeta();
+				m.setLore(Arrays.asList("Stored XP: " + (onPoints + points)));
+				m.setDisplayName(ChatColor.RESET + "Honey Xp Bottle");
+				// TODO no hack? For now we just add random enchantment and remove its visisbility:
+				m.addEnchant(Enchantment.THORNS, 1, true);
+				m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+				// Give the item.
+				newB.setItemMeta(m);
+				e.getPlayer().getInventory().addItem(newB);
+				e.getPlayer().updateInventory();
+				e.getPlayer().getWorld().playSound(e.getPlayer(), Sound.BLOCK_BUBBLE_COLUMN_UPWARDS_INSIDE, 1.0f, 1.0f);
+				return;
 			}
+			e.getPlayer().getWorld().playSound(e.getPlayer(), Sound.BLOCK_LAVA_POP, 1.0f, 1.0f);
 		}
 	}
 	
